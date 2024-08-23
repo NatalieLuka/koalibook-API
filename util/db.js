60b29bc649212d6a1965da0e6fd0ffe80b227100
Ralf Siewert <ralf@actyvy.st>
2024-08-23 10:30:48 +0200

    change postgres connection method to connection string

--- util/db.js
+++ util/db.js
@@ -3,13 +3,14 @@ import knex from "knex";
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: config.POSTGRES_HOST,
-    port: config.POSTGRES_PORT,
-    user: config.POSTGRES_USER,
-    password: config.POSTGRES_PASSWORD,
-    database: config.POSTGRES_DATABASE,
-  },
+  connection: process.env.PG_CONNECTION_STRING,
+  // connection: {
+  //   host: config.POSTGRES_HOST,
+  //   port: config.POSTGRES_PORT,
+  //   user: config.POSTGRES_USER,
+  //   password: config.POSTGRES_PASSWORD,
+  //   database: config.POSTGRES_DATABASE,
+  // },
 });
 
 export default db;
